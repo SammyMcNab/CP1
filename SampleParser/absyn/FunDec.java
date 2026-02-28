714a6d870f99b8f5fa41b93ed24d2ea4ee734202
mkuzmano <mkuzmano@uoguelph.ca>
2026-02-28 15:23:24 -0500

    added placeholders

--- SampleParser/absyn/FunDec.java
+++ SampleParser/absyn/FunDec.java
@@ -3,15 +3,15 @@ package absyn;
 public class FunDec extends Dec {
   public String name;
   public Type type;
-  public String stmt;
+  public Exp body;
   public VarDecList params;
 
-  public FunDec( int row, int col, Type type, String name, VarDecList params, String stmt ) {
+  public FunDec( int row, int col, Type type, String name, VarDecList params, String body ) {
     this.row = row;
     this.col = col;
     this.type = type;
     this.name = name;
-    this.stmt = stmt;
+    this.body = body;
     this.params = params;
 
   }
